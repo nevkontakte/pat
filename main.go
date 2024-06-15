fe85bc10b9326e4e32ba1a9d085b361f78b3c9c0
Nevkontakte <aleks@nevkontakte.com>
2024-06-15 18:12:10 +0100

    Establish a Postgres connection on start and log database version.

--- main.go
+++ main.go
@@ -6,16 +6,20 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/gommon/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var (
 	bind = flag.String("bind", ":8080", "Address to start the HTTP server at.")
-	db   = flag.String("db", "", "Database connection string.")
+	db   = flag.String("db", "host=localhost user=postgres password=postgres dbname=pat port=5432 sslmode=disable", "Database connection string.")
 )
 
 func main() {
 	// Echo instance
 	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
 
 	// Middleware
 	e.Use(middleware.Logger())
@@ -23,6 +27,14 @@ func main() {
 
 	// Routes
 	e.GET("/", hello)
+
+	db, err := gorm.Open(postgres.Open(*db), &gorm.Config{})
+	if err != nil {
+		e.Logger.Fatalf("Failed to connect to the database at %q: %v", err)
+	}
+	var version string
+	db.Raw("SELECT version();").Scan(&version)
+	e.Logger.Infof("Connected to %s.", version)
 
 	// Start server
 	e.Logger.Fatal(e.Start(*bind))
